dab847edba98c1333aafb6f796edd4c517a1b04c
NeroRT56 <nrodriguez980@soyudemedellin.edu.co>
2023-11-01 14:34:41 -0500

    Actualizo en mi dispositivo el proyecto

--- sistema de alquiler de bicicletas/sistema de alquiler de bicicletas/interfax-grafica.py
+++ sistema de alquiler de bicicletas/sistema de alquiler de bicicletas/interfax-grafica.py
@@ -12,7 +12,6 @@ class MainWindowSistemaBicicleta(QMainWindow):
         self.setFixedSize(self.size())
         self.dialogo_agregar_usuario = DialogoUsuariosAdd()
         self.dialogo_agregar_bicicletas = DialogoBicicletasAdd()
-        # El importa la tienda, no se que importar
         self.runin =BikeRentalSystem()
         self.__configurar()
         self.__cargar_datos_usuario()
@@ -25,19 +24,22 @@ class MainWindowSistemaBicicleta(QMainWindow):
         self.listViewUsuariosDisponibles.setModel(QStandardItemModel())
         self.listViewBicicletasDisponibles.setModel(QStandardItemModel())
         # Este metodo se va a ejecutar cada que el usuario seleccione un item de la lista.
-        self.listViewUsuariosDisponibles.selectionModel().selectionChanged.connect(self.obternerItemSeleccionado)
+        self.listViewUsuariosDisponibles.selectionModel().selectionChanged.connect(self.obternerItemSeleccionadoUsuario)
         #este metodo se va a ejecutar cada que el usuario seleccione un item de las bicicletas
         self.listViewBicicletasDisponibles.selectionModel().selectionChanged.connect(self.obternerItemSeleccionadoBicicletas)
-
         #enlazar los eventos de los botones /pushButtonAgrega_Nuevo_usuario /pushButtonAgregar_Nueva_Bicicleta / 
         self.pushButtonAgrega_Nuevo_usuario.clicked.connect(self.abrir_dialogo_Agregar_usuario)
         self.pushButtonAgregar_Nueva_Bicicleta.clicked.connect(self.abrir_dialogo_Agregar_bicicletas)
         # Desde aca se utiliza para eliminar un item
         self.pushButtonEliminar_Usuario.clicked.connect(self.eliminarItemSeleccionadoUsuario)
         self.pushButtonEliminar_Bicicleta.clicked.connect(self.eliminarItemSeleccionadoBicicleta)
+        # enlazar con la funcion de rentar bicicleta 
+        self.pushButtonAlquilar_Bicicleta.clicked.connect(self.obtenerRentaBicicletas)
+        #devolver la renta de la factura 
+        self.pushButtonDevolver_Bicicleta.clicked.connect(self.devolverRentaBicicletas)
 
     #este metodo se va a ejecutar cada que el usuario le de click a un item de la lista 
-    def obternerItemSeleccionado(self):
+    def obternerItemSeleccionadoUsuario(self):
         #metodo retorna el indice de la lista del elemento que se selecciono
         indice_seleccionado = self.listViewUsuariosDisponibles.currentIndex()
         if indice_seleccionado.isValid():  # Aca se verifica que el indice sea valido, (que no sea negativo y demas validaciones)
@@ -53,32 +55,28 @@ class MainWindowSistemaBicicleta(QMainWindow):
             # imprimo el objeto user del item seleccionado. Este objeto user se asigna en la linea 56 de este archivo
             print(f"Elemento seleccionado: {self.bicicleta_seleccionada.bicis}")
     def eliminarItemSeleccionadoUsuario(self):
-        currentIndex = self.listViewUsuariosDisponibles.currentRow()
-
-        item = self.listViewUsuariosDisponibles.item(currentIndex)
-        if item is None:
+        usuario = self.usuario_seleccionado.user
+        if usuario is None:
             return
-        question = QMessageBox.question(self,"¿ Deseas eliminar un usuario ?" + item.text(), QMessageBox.yes | QMessageBox.no)
-        if question == QMessageBox.yes:
-            item = self.listViewUsuariosDisponibles.takeItem(currentIndex)
-            del item
-
+        question =  question =QMessageBox.question(self,"Eliminar","¿Deseas eliminar el usuario{}".format(usuario.name),QMessageBox.Yes | QMessageBox.No)       
+        if question == QMessageBox.Yes:
+            del usuario
         #aqui se utiliza el metodo para eliminar un item del usuario 
     def eliminarItemSeleccionadoBicicleta(self):
         bicicleta = self.bicicleta_seleccionada.bicis
-        currentIndex = self.listViewBicicletasDisponibles.currentRow(bicicleta)
-        
-        item =self.listViewBicicletasDisponibles(currentIndex())
-         
-        
-        if item is None:
+        if bicicleta is None:
             return
-        question = QMessageBox.question(self,"¿ Deseas eliminar un usuario ?" + item.text() , QMessageBox.yes | QMessageBox.no)
-        if question == QMessageBox.yes:
-            item = self.listViewUsuariosDisponibles.takeItem(currentIndex)
-            del item
-        
+        question = question = QMessageBox.question(self,"Eliminar","¿Deseas eliminar la bicicleta {}?".format(bicicleta.name) , QMessageBox.Yes | QMessageBox.No)
+        if question == QMessageBox.Yes:
+            del bicicleta
 
+    def obtenerRentaBicicletas(self):
+        #aquí esta la funcion de rentar bicicleta 
+        pass
+
+    def devolverRentaBicicletas(self):
+        #Aqui irá el metodo para devolver la bicicleta y pagar 
+        pass
 
     def __cargar_datos_usuario(self):
         # se quiere cargar los datos del catalogo de usuarios
@@ -119,13 +117,13 @@ class MainWindowSistemaBicicleta(QMainWindow):
             available = True
             modell = self.dialogo_agregar_bicicletas.lineEditModel.text()
             newBike=Bike(id, name, description, price, available, modell)
-            item = QStandardItem(str(newBike))
             self.runin.agregar_bike(newBike)
+            item = QStandardItem(str(newBike))
+            item.bicis = newBike
             item.setEditable(False)
             self.listViewBicicletasDisponibles.model().appendRow(item)
         self.dialogo_agregar_bicicletas.limpiar()
 
-    
 #comenzamos con los dialogos 
 class DialogoUsuariosAdd(QDialog):
     def __init__(self):
@@ -136,9 +134,6 @@ class DialogoUsuariosAdd(QDialog):
         self.setFixedSize(self.size())
         self.__configurar()
     def __configurar(self):
-        #aquí configuramos las validaciones de los line edit de los tipo de caracteretes, o enteron, floar va a recibir 
-        #self.lineEditNombre.setValidator(QRegExpValidator(QRegExp("\\d{6}"), self.lineEditNombre)) # para resctificar las cadenas de caracteres dentro de line edit
-        #self.lineEditCorreo.setValidator(QRegExpValidator(QRegExp("\\d{6}"), self.lineEditCorreo)) # 28:00
         self.lineEditCelular.setValidator(QRegExpValidator(QRegExp("\\d{11}"), self.lineEditCelular))
 
     def limpiar(self):
@@ -151,7 +146,6 @@ class DialogoUsuariosAdd(QDialog):
 
     def accept(self) -> None:
         #aquí verificamos que se halla llenado todos los datos solicitados 
-      
         if self.lineEditNombre.text() != "" and self.lineEditCorreo.text() != "" and self.lineEditCelular.text() != "":
            
             super().accept()
@@ -164,7 +158,6 @@ class DialogoUsuariosAdd(QDialog):
             msg_box.setStandardButtons(QMessageBox.Ok)
             msg_box.exec()
 
-
 class DialogoBicicletasAdd(QDialog):
     def __init__(self):
         QDialog.__init__(self)
@@ -173,6 +166,7 @@ class DialogoBicicletasAdd(QDialog):
         #para que la ventana no se pueda maximizar o minimizar
         self.setFixedSize(self.size())
         self.__configurar()
+
     def __configurar(self):
         #aquí configuramos las validacion de  los line edit de los tipo de caracteretes, o enteron, floar va a recibir 
         #corregir 
@@ -192,11 +186,7 @@ class DialogoBicicletasAdd(QDialog):
         self.lineEditDescription.clear()
         self.lineEditPrice.clear()
         self.lineEditModel.clear()
-
-
-
-
-    
+ 
     def accept(self) -> None:
         #aquí verificamos que se halla llenado todos los datos solicitados 
         if self.lineEditID.text() != "" and self.lineEditName.text() != "" and self.lineEditDescription.text() != "" and self.lineEditPrice.text() != "" and self.lineEditModel.text() != "":
